929fb4ea2d2819d83485a43b20db9029164e7301
Mikooo <mikooomichael@gmail.com>
2025-05-15 20:32:52 -0400

    sake: Import radio firmware from proprietary_vendor_firmware
    * Equivilant blobs to 33.0210.0210.296
    * Reference: 4d5a92088662a1a167a3ceff699b5ef78a344134 (https://gitlab.com/the-muppets/proprietary_vendor_firmware/-/tree/lineage-20)
    
    Change-Id: I1601ba9bfcd86a4039df4c6dd2c3913625a9fe77

--- Android.mk
+++ Android.mk
@@ -3,3 +3,27 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),sake)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,46f6d4535c45132c118279d805418cc5b5a6ab71)
+$(call add-radio-file-sha1-checked,radio/aop.img,f3d42899a2556ad43c3afdb6d43ea69b75e31ebf)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,05a3801ac2bae580d336ac03cb7725fc682ea9d1)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,c7ef439dc545af976664344dc6b5ea6b534907a7)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,311382340503cd0df3a3320982b05f33b1dce5f5)
+$(call add-radio-file-sha1-checked,radio/dsp.img,be677e0ce81da9196a80d29370b85d3e452b41fb)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,0fd321b441f7632ed739695048084c0fa6fbfcbe)
+$(call add-radio-file-sha1-checked,radio/hyp.img,32282901d20bde706f1286e53bad081f8513cd5d)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,bb4bfdfc285a9e1351f21496525f5f461cb8207b)
+$(call add-radio-file-sha1-checked,radio/modem.img,9bb5f20022343f1866bb9003aed9f3312dbbbf0d)
+$(call add-radio-file-sha1-checked,radio/multiimgoem.img,99e0abe6cf5c1e33ea5b68460b3c36fcefcb2930)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,9f6e519ef9ee3753bfdecfe3bc69a8ed67f943b0)
+$(call add-radio-file-sha1-checked,radio/qweslicstore.img,180cf2903b9872499d05eeef08c472575fa3225e)
+$(call add-radio-file-sha1-checked,radio/shrm.img,a80107a29cbb4f2af04073ea41c039dc8a02c244)
+$(call add-radio-file-sha1-checked,radio/tz.img,1a09972b0cd4e8d1e27e1bfbad8ae1d0ca46fa6b)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,4c14098bee8d7f8d70d62fec4e09a96902900730)
+$(call add-radio-file-sha1-checked,radio/xbl.img,e09c2ab52f99bb137a8d2742ee5e8a1fda6e5224)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,d4238d4cf29be7b9530e863162bcb791a0c4ab26)
+$(call add-radio-file-sha1-checked,radio/xrom.img,94f4a720b243fd2621416165b4b0281c4e4d873c)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,24 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    abl \
+    aop \
+    bluetooth \
+    cpucp \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    keymaster \
+    modem \
+    multiimgoem \
+    qupfw \
+    qweslicstore \
+    shrm \
+    tz \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xrom
